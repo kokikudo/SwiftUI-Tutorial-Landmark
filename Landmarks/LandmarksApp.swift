d6711400e519ba9e7b09c7b2123bcfbfcb430261
kokikudo <massanmesu@icloud.com>
2022-01-11 20:52:36 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by kudo koki on 2022/01/11.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
